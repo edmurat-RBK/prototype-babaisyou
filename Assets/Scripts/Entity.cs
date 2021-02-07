2737c8c02da86832ef4740dc44c10667cdf31ff8
Edouard Murat <edouard.murat59@gmail.com>
2021-02-07 01:29:34 +0100

    Added input mapping

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    void YouBehaviour()
+    {
+        Debug.Log("Run YouBehaviour");
+    }
+
+    void WinBehaviour()
+    {
+        Debug.Log("Run WinBehaviour");
+    }
+}
