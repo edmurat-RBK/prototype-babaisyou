abba5fb86c68a7fc464fd59840a8b24c3e4b2a33
Edouard Murat <e.murat@rubika-edu.com>
2021-02-09 00:42:14 +0100

    Added Rule IS

---
+++ Assets/Scripts/Rules/RuleIs.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleIs : Rule
+{
+    public override void PerformStep()
+    {
+        // For both direction : up/bottom and left/right
+
+        // Check if there is a word on the adjacent tile
+
+        // If there is one valid, check at opposite position
+
+
+    }
+}
